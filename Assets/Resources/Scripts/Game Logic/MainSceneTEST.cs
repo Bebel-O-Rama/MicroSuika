ffc951d5d71d1d9982fcd69729f9973785eea891
Bebel-O-Rama <bebel-o-rama@hotmail.com>
2024-01-13 18:00:08 -0500

    Cleaned the script MainSceneTEST.cs before changing the spawning method

--- Assets/Resources/Scripts/Game Logic/MainSceneTEST.cs
+++ Assets/Resources/Scripts/Game Logic/MainSceneTEST.cs
@@ -14,35 +14,14 @@ public class MainSceneTEST : MonoBehaviour
     
     private void OnEnable()
     {
-        Debug.Log("Start In");
         foreach (var playerData in gameData.playerDataList)
         {
+            if (!playerData.IsPlayerConnected())
+                break;
             var playerObj = PlayerInput.Instantiate(playerPf, playerData.playerIndexNumber, pairWithDevice: playerData.inputDevice);
             var player = playerObj.GetComponentInParent<Player>();
-            // var (playerIndex, player) = gameData.AddPlayer(playerObj.GetComponentInParent<Player>());
-            // var scores = gameData.playerDataList[i].mainScore;
+
             player.InitializePlayer(playerData, gameData.lobby);
-        
-            // Do custom stuff when a player joins in the lobby
-            // Color randColor = Color.HSVToRGB(Random.Range(0f, 1f), 0.6f, 1f);
-            // AddPlayerJoinPopup(playerIndex, player, randColor);
-            // player.UpdateMainCannonColor(randColor);
-            // ConnectToLobbyScore(gameData.GetPlayerScoreReferences(playerIndex).mainScore, lobbyScore[playerIndex-1], randColor);
         }
-        Debug.Log("Start Out");
     }
-
-    private void Start()
-    {
-        // Debug.Log("In OnEnable");
-        // for (int i = 0; i < players.Count - 1; ++i)        {
-        //     players[i].playerInputHandler._playerInput.user = gameData.inputUsers[i];
-        // }
-        // Debug.Log("OnEnable Out");
-    }
-
-    // private void Update()
-    // {
-    //     throw new NotImplementedException();
-    // }
 }
