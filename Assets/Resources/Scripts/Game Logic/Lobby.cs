e6082c0f501b3e2586a1dae1eaba2f594285eb0e
Bebel-O-Rama <bebel-o-rama@hotmail.com>
2024-01-13 18:42:34 -0500

    Removed useless code in the ball scripts and fixed the player number for the OnJoin popup

--- Assets/Resources/Scripts/Game Logic/Lobby.cs
+++ Assets/Resources/Scripts/Game Logic/Lobby.cs
@@ -64,7 +64,7 @@ public class Lobby : MonoBehaviour
         var popup = Instantiate(onJoinPopup, player.GetCannon(true).transform);
         var tmp = popup.GetComponent<TextMeshPro>();
         tmp.color = randColor;
-        tmp.text = $"P{playerIndex}";
+        tmp.text = $"P{playerIndex + 1}";
     }
 
     private void UpdateLobbyTriggers(int newPlayerNumber)
