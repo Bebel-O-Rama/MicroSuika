a6502e08e3f3469fb2c5cbd23d70e8e4289415f4
Nicolas A-L <bebel-o-rama@hotmail.com>
2024-03-26 14:43:44 -0400

    Updated the sprites for the background
    - Now with 5 layers!

--- Assets/Scripts/UI/BackgroundMovement.cs
+++ Assets/Scripts/UI/BackgroundMovement.cs
@@ -4,23 +4,22 @@ namespace MultiSuika.UI
 {
     public class BackgroundMovement : MonoBehaviour
     {
-        [Range(0f, 2 * Mathf.PI)] [SerializeField]
-        public float movementDirectionAngle;
-
-        [Min(0f)] [SerializeField] public float movementSpeed;
-        static readonly int MainTex = Shader.PropertyToID("_MainTex");
+        [SerializeField] [Range(0f, 2f * Mathf.PI)] private float _movementDirectionAngle;
+        [SerializeField] [Min(0f)] private float movementSpeed;
+        
+        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
         private Renderer _renderer;
         private Vector2 _directionalMovement;
 
         private void Awake()
         {
             _renderer = GetComponent<Renderer>();
-            _directionalMovement = new Vector2(Mathf.Cos(movementDirectionAngle), Mathf.Sin(movementDirectionAngle));
+            _directionalMovement = new Vector2(Mathf.Cos(_movementDirectionAngle), Mathf.Sin(_movementDirectionAngle));
         }
 
         private void LateUpdate()
         {
-            _directionalMovement = new Vector2(Mathf.Cos(movementDirectionAngle), Mathf.Sin(movementDirectionAngle));
+            _directionalMovement = new Vector2(Mathf.Cos(_movementDirectionAngle), Mathf.Sin(_movementDirectionAngle));
 
             var currentOffset = _renderer.material.GetTextureOffset(MainTex);
             var newOffset = currentOffset + (movementSpeed / 15f) * Time.deltaTime * _directionalMovement;
