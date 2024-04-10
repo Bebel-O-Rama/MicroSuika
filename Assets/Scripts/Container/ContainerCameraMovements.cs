977a4ed5371ec9542c6f64277e99d21e44549565
Nicolas A-L <bebel-o-rama@hotmail.com>
2024-04-10 12:59:14 -0400

    Added a shake when a ball hits the top of a container
    Also set the ActiveInputHandler to "Both" (not only the new system)

--- Assets/Scripts/Container/ContainerCameraMovements.cs
+++ Assets/Scripts/Container/ContainerCameraMovements.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using MultiSuika.Ball;
 using MultiSuika.Manager;
 using MultiSuika.Utilities;
 using UnityEngine;
@@ -6,21 +8,35 @@ namespace MultiSuika.Container
 {
     public class ContainerCameraMovements : MonoBehaviour
     {
-        [Header("Camera parameters")]
+        [Header("Camera parameters")] 
         [SerializeField] private Camera _camera;
         [SerializeField] private Transform _mainVerticalTransform;
         [SerializeField] private Transform _secondaryTransform;
-        
         [SerializeField] private float _yMinHeight;
         [SerializeField] private float _yMaxHeight;
+
+        [Header("Screen Shake parameters (OnContainerHit)")]
+        [SerializeField] private float _hitDuration = 0.5f;
+        [SerializeField] private Vector3 _hitStrength;
+        [SerializeField] private int _hitVibrato = 20;
+        [SerializeField] private float _hitRandomness = 90f;
+        [SerializeField] private bool _hitFadeOut = true;
+        [SerializeField] private ShakeRandomnessMode _hitMode = ShakeRandomnessMode.Full;
+        [SerializeField] private bool _isTestingEnabled = false;
+        
         
         private int _playerIndex;
         private FloatReference _normalizedVerticalPosition;
+        private Tweener _tweener;
         
+        
+
         private void Start()
         {
             var container = GetComponentInParent<ContainerInstance>();
             _playerIndex = ContainerTracker.Instance.GetPlayerFromItem(container);
+            ContainerTracker.Instance.OnContainerHit.Subscribe(ContainerHitShake, _playerIndex);
+
             _normalizedVerticalPosition = ScoreManager.Instance.GetNormalizedSpeedReference(_playerIndex);
             _camera.cullingMask |= (1 << gameObject.layer);
         }
@@ -29,6 +45,29 @@ namespace MultiSuika.Container
         {
             var newYPos = _yMinHeight + _normalizedVerticalPosition * (_yMaxHeight - _yMinHeight);
             _mainVerticalTransform.position = new Vector3(0, -newYPos, 0);
+            
+            if (_isTestingEnabled && Input.GetKeyDown(KeyCode.B))
+                ContainerHitShakeTest();
+        }
+
+        private void ContainerHitShake((BallInstance ball, ContainerInstance container) args)
+        {
+            if (_tweener.IsActive())
+            {
+                _tweener.Restart();
+                return;
+            }
+            _tweener = _secondaryTransform.DOShakePosition(_hitDuration, _hitStrength, _hitVibrato, _hitRandomness, fadeOut: _hitFadeOut, randomnessMode: _hitMode);
+        }
+        
+        private void ContainerHitShakeTest()
+        {
+            if (_tweener.IsActive())
+            {
+                _tweener.Restart();
+                return;
+            }
+            _tweener = _secondaryTransform.DOShakePosition(_hitDuration, _hitStrength, _hitVibrato, _hitRandomness);
         }
     }
 }
