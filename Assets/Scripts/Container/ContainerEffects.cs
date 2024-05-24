5f8fff2c5ada6dcd81567a60be403b6f284591bb
Nicolas A-L <bebel-o-rama@hotmail.com>
2024-05-24 13:31:18 -0400

    Added lead adaptive feedback

--- Assets/Scripts/Container/ContainerEffects.cs
+++ Assets/Scripts/Container/ContainerEffects.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using MultiSuika.Manager;
 using UnityEngine;
@@ -10,13 +10,27 @@ namespace MultiSuika.Container
     {
         [Header("VFXs")] 
         [SerializeField] private ParticleSystem _speedLines;
-        [SerializeField] private ParticleSystem _glowEffect;
         [SerializeField] private SpriteRenderer _winOutsideSprite;
         [SerializeField] private ParticleSystem _loseExplosion;
+        [SerializeField] private ParticleSystem _glowEffect;
+
+        [Header("Lead Parameters")]
+        [SerializeField] private float _speedLinesMinScale; // 3
+        [SerializeField] private float _speedLinesMaxScale; // 5
+        [SerializeField] private float _speedLinesMinRateOverEmission; // 100
+        [SerializeField] private float _speedLinesMaxRateOverEmission; // 250
         
+        [SerializeField] private float _glowDuration;
+
         private int _playerIndex;
 
-        
+        private Sequence _speedLinesSequence;
+
+        private void Awake()
+        {
+            _speedLinesSequence = DOTween.Sequence();
+        }
+
         private void Start()
         {
             _playerIndex = ContainerTracker.Instance.GetPlayerFromItem(GetComponentInParent<ContainerInstance>());
@@ -25,30 +39,76 @@ namespace MultiSuika.Container
             VersusManager.Instance.OnLeadStop.Subscribe(OnLeadStop, _playerIndex);
             VersusManager.Instance.OnGameOver.Subscribe(OnGameOver, _playerIndex);
         }
-        
+
+        #region GameOver
+
         private void OnGameOver(bool hasWon)
         {
+            if (_speedLinesSequence.IsPlaying())
+            {
+                _speedLinesSequence.Kill();
+            }
+            _speedLines.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            _glowEffect.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+
             if (hasWon)
             {
-                if (_winOutsideSprite)
-                    _winOutsideSprite.DOFade(1, 1);
+                OnWin();
             }
             else
             {
-                if (_loseExplosion)
-                    _loseExplosion.Play();
+                OnLose();
             }
         }
-        
-        private void OnLeadStart(float timerDuration)
+
+        private void OnWin()
         {
-            _speedLines.Play();
-        }
-        
-        private void OnLeadStop(bool x)
-        {
-            _speedLines.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            _winOutsideSprite.DOFade(1, 1);
         }
 
+        private void OnLose()
+        {
+            _loseExplosion.Play();
+        }
+
+        #endregion
+
+        #region Lead
+
+        private void OnLeadStart(float timerDuration)
+        {
+            if (_speedLinesSequence.IsPlaying())
+            {
+                _speedLinesSequence.Kill();
+            }
+            _speedLinesSequence = DOTween.Sequence();
+            
+            // For now, let's say that the minimum timer duration will safeguard us for any issue there
+            var speedLinesRampUpDuration = Mathf.Max(timerDuration - _glowDuration, 1);
+
+            var speedLineEmission = _speedLines.emission;
+            
+            _speedLines.transform.localScale = Vector3.one * _speedLinesMinScale;
+            speedLineEmission.rateOverTime = _speedLinesMinRateOverEmission;
+            
+            _speedLinesSequence.AppendCallback(() => _speedLines.Play())
+                .Append(_speedLines.transform.DOScale(_speedLinesMaxScale, speedLinesRampUpDuration))
+                .Join(DOTween.To(() => speedLineEmission.rateOverTime.constant,
+                    x => speedLineEmission.rateOverTime = x, _speedLinesMaxRateOverEmission,
+                    speedLinesRampUpDuration))
+                .AppendCallback(() => _glowEffect.Play());
+        }
+
+        private void OnLeadStop(bool x)
+        {
+            if (_speedLinesSequence.IsPlaying())
+            {
+                _speedLinesSequence.Kill();
+            }
+            _speedLines.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            _glowEffect.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+
+        #endregion
     }
 }
