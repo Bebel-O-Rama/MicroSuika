6207ce9760d7a3cc2868aa1e8bc6cc08d5d52ffa
Nicolas A-L <bebel-o-rama@hotmail.com>
2024-07-18 12:38:55 -0400

    Cleaned up elements for the WatermarkPicker

--- Assets/Scripts/Utilities/WatermarkPicker.cs
+++ Assets/Scripts/Utilities/WatermarkPicker.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-namespace MultiSuika
+namespace MultiSuika.Utilities
 {
     public class WatermarkPicker : MonoBehaviour
     {
